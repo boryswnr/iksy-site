ad9ec983762fa129b18b67302b34d457ba403d38
boryswnr <boryswnr@protonmail.com>
2023-01-24 18:34:46 +0100

    sliding photos with dragging

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,11 +9,11 @@ hamburger.addEventListener("click", () => {
 });
 
 // bands photo slider
+const slider = document.querySelector(".band-photos-wrapper");
 const photoList = document.querySelectorAll(".photo-container");
-let displayedPhoto = 1;
+let currentIndex = 0;
 
-// spreading photos so they are next to each other,
-// adjusted for -50% so the photos are centered
+// spreading photos so they are next to each other
 photoList.forEach((photo, index) => {
     photo.style.transform = `translateX(${index * 100}%)`;
 });
@@ -21,10 +21,8 @@ photoList.forEach((photo, index) => {
 // handling switching photos
 const switchArray = document.querySelectorAll(".switch");
 const switchClickHandler = (e) => {
-    const multiplier = +e.target.id - 1;
-    photoList.forEach((slide, index) => {
-        slide.style.transform = `translateX(${100 * (index - multiplier)}%)`;
-    });
+    currentIndex = +e.target.id - 1;
+    slidePhotos();
     switchArray.forEach((el) => {
         el.classList.remove("active");
     });
@@ -34,3 +32,92 @@ const switchClickHandler = (e) => {
 switchArray.forEach((elem) =>
     elem.addEventListener("click", (e) => switchClickHandler(e))
 );
+const slidePhotos = () => {
+    photoList.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`;
+    });
+    switchArray.forEach((element, index) => {
+        element.classList.remove("active");
+        if (index === currentIndex) element.classList.add("active");
+    });
+};
+// switching photos with sliding
+
+// set up our state
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0;
+
+photoList.forEach((slide, index) => {
+    const slideImage = slide.querySelector("img");
+    slideImage.addEventListener("dragstart", (e) => e.preventDefault());
+
+    // Touch events
+    slide.addEventListener("touchstart", touchStart(index));
+    slide.addEventListener("touchend", touchEnd);
+    slide.addEventListener("touchmove", touchMove);
+
+    // Mouse events
+    slide.addEventListener("mousedown", touchStart(index));
+    slide.addEventListener("mouseup", touchEnd);
+    slide.addEventListener("mouseleave", touchEnd);
+    slide.addEventListener("mousemove", touchMove);
+});
+
+// Disable context menu
+window.oncontextmenu = function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+};
+
+function touchStart(index) {
+    return function (event) {
+        currentIndex = index;
+        startPos = getPositionX(event);
+        isDragging = true;
+
+        // https://css-tricks.com/using-requestanimationframe/
+        animationID = requestAnimationFrame(animation);
+    };
+}
+
+function touchEnd() {
+    isDragging = false;
+    cancelAnimationFrame(animationID);
+
+    const movedBy = currentTranslate - prevTranslate;
+
+    if (movedBy < -100 && currentIndex < photoList.length - 1)
+        currentIndex += 1;
+
+    if (movedBy > 100 && currentIndex > 0) currentIndex -= 1;
+
+    setPositionByIndex();
+}
+
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event);
+        currentTranslate = prevTranslate + currentPosition - startPos;
+    }
+}
+
+function getPositionX(event) {
+    return event.type.includes("mouse")
+        ? event.pageX
+        : event.touches[0].clientX;
+}
+
+function animation() {
+    slidePhotos();
+    if (isDragging) requestAnimationFrame(animation);
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * -window.innerWidth;
+    prevTranslate = currentTranslate;
+    slidePhotos();
+}
