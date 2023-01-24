fcd5afa7e59d729e605c49e282fa50fa97f022d8
boryswnr <boryswnr@protonmail.com>
2023-01-24 17:04:50 +0100

    added functional but yet unstyled carousel slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// mobile menu
 const hamburger = document.getElementById("hamburger-btn");
 const mobileMenu = document.getElementById("small-screen-menu");
 
@@ -6,3 +7,33 @@ hamburger.addEventListener("click", () => {
     mobileMenu.classList.toggle("visible");
     document.body.classList.toggle("stop-scroll");
 });
+
+// bands photo slider
+const photoList = document.querySelectorAll(".photo-container");
+let displayedPhoto = 1;
+// const photoWrapper = document.querySelector(".band-photos-wrapper");
+
+// spreading photos so they are next to each other,
+// adjusted for -50% so the photos are centered
+photoList.forEach((photo, index) => {
+    // photo.style.transform = `translate(calc(-50% + ${index * 100}%), 0)`;
+    photo.style.transform = `translateX(${index * 100}%)`;
+});
+
+// handling switching photos
+const switchArray = document.querySelectorAll(".switch");
+const switchClickHandler = (e) => {
+    const multiplier = +e.target.id - 1;
+    // photoWrapper.style.transform = `translateX(${-100 * multiplier}%)`;
+    photoList.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - multiplier)}%)`;
+    });
+    switchArray.forEach((el) => {
+        el.classList.remove("active");
+    });
+    e.target.classList.add("active");
+};
+
+switchArray.forEach((elem) =>
+    elem.addEventListener("click", (e) => switchClickHandler(e))
+);
